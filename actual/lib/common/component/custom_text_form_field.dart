8ee2782ac5dd93def0cbca79e87b2f640310c75d
AsherKim <tawans@naver.com>
2023-07-22 02:43:38 +0900

    🎉 Feat: Begin a Project

---
+++ actual/lib/common/component/custom_text_form_field.dart
@@ -0,0 +1,57 @@
+import 'package:actual/common/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final String? hintText;
+  final String? errorText;
+  final bool obscureText;
+  final bool autofocus;
+  final ValueChanged<String>? onChanged;
+
+  const CustomTextFormField({
+    super.key,
+    required this.onChanged,
+    this.errorText,
+    this.hintText,
+    this.autofocus = false,
+    this.obscureText = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final baseBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(5),
+      borderSide: const BorderSide(
+        color: INPUT_BORDER_COLOR,
+        width: 1.0,
+      ),
+    );
+
+    return TextFormField(
+      onChanged: onChanged,
+      autofocus: autofocus,
+      obscureText: obscureText,
+      cursorColor: PRIMARY_COLOR,
+      decoration: InputDecoration(
+        contentPadding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 15,
+        ),
+        hintText: hintText,
+        hintStyle: const TextStyle(
+          color: BODY_TEXT_COLOR,
+          fontSize: 14,
+        ),
+        errorText: errorText,
+        fillColor: INPUT_BG_COLOR,
+        filled: true,
+        border: baseBorder,
+        focusedBorder: baseBorder.copyWith(
+          borderSide: baseBorder.borderSide.copyWith(
+            color: PRIMARY_COLOR,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ image_carousel/android/app/src/main/kotlin/me/asherdev/image_carousel/MainActivity.kt
@@ -0,0 +1,6 @@
+package me.asherdev.image_carousel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
