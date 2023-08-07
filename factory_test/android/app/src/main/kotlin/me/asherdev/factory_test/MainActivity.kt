70f9b465327048fabb6345759e5f1a4c5315c253
AsherKim <tawans@naver.com>
2023-08-08 01:09:44 +0900

    🎉 Begin a Project

---
+++ factory_test/android/app/src/main/kotlin/me/asherdev/factory_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package me.asherdev.factory_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ factory_test/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Demo Home Page'),
+          backgroundColor: Colors.deepOrange,
+        ),
+        body: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'This is my first Flutter app!',
+                  style: TextStyle(fontSize: 24),
+                ),
+                CircularProgressIndicator(
+                  valueColor:
+                      AlwaysStoppedAnimation<Color>(Colors.orangeAccent),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
